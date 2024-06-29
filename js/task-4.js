a745f87af79d1dad8a5829fa85a16faf020d11d5
alena_sinitsa <alenasinitsa85@gmail.com>
2024-06-29 14:22:31 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(`form.login-form`);
+// console.dir(form);
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let inputEmail = form.elements.email.value.trim();
+  let inputPassword = form.elements.password.value.trim();
+
+  if (inputEmail === "" || inputPassword === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  let formObj = {
+    email: inputEmail,
+    password: inputPassword,
+  };
+
+  console.log(formObj);
+
+  form.reset();
+}
