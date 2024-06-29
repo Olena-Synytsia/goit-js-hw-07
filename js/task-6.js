025623b47e97cf9511b7c76a0864652a6a9b70f6
alena_sinitsa <alenasinitsa85@gmail.com>
2024-06-30 00:41:31 +0300

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNumber = document.querySelector('input[type="number"]');
+const btnCreate = document.querySelector(".btn-create");
+const btnDestroy = document.querySelector(".btn-destroy");
+
+function createBoxes(amount) {
+  const boxContainer = document.getElementById("boxes");
+  boxContainer.innerHTML = "";
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxContainer = document.getElementById("boxes");
+  boxContainer.innerHTML = "";
+}
+
+btnCreate.addEventListener("click", function () {
+  const amount = parseInt(inputNumber.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    inputNumber.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+btnDestroy.addEventListener("click", function () {
+  destroyBoxes();
+});
