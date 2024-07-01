63af44173d3560d5b513511a455f522c9146425b
alena_sinitsa <alenasinitsa85@gmail.com>
2024-07-01 12:45:10 +0300

    correct task-6

--- js/task-6.js
+++ js/task-6.js
@@ -12,15 +12,19 @@ function createBoxes(amount) {
   const boxContainer = document.getElementById("boxes");
   boxContainer.innerHTML = "";
 
+  const fragment = document.createDocumentFragment();
+
   let size = 30;
-  for (let i = 0; i < amount; i++) {
+  for (let i = 0; i < amount; i += 1) {
     const box = document.createElement("div");
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxContainer.appendChild(box);
+    fragment.appendChild(box);
     size += 10;
   }
+
+  boxContainer.appendChild(fragment);
 }
 
 function destroyBoxes() {
